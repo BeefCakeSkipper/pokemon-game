a2456cf41372ceba9ecd5c94546548b3771df635
BeefCakeSkipper <skippy.smahon@gmail.com>
2024-07-24 17:48:47 +0800

    added boundaries

--- index.js
+++ index.js
@@ -3,10 +3,8 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width = window.innerWidth - 25
+canvas.width = window.innerWidth
 canvas.height = window.innerHeight
-c.fillStyle = 'white'
-c.fillRect(0, 0, canvas.width, canvas.height)
 
 const image = new Image()
 image.src = './img/Pellet Town.png'
@@ -16,21 +14,87 @@ console.log(image);
 const playerImage = new Image()
 playerImage.src = './img/playerDown.png'
 
+const collisionsMap = []
+for (let i = 0; i < collisions.length; i += 70) {
+    collisionsMap.push(collisions.slice(i, 70 + i))
+}
+
+const offset = {
+    x: 30,
+    y: -200
+}
+
 //Classes
-class Sprite {
-    constructor({ position, velocity, image }) {
+class Boundary {
+    static width = 48
+    static height = 48
+    constructor({ position }) {
         this.position = position
-        this.image = image
+        this.width = 48
+        this.height = 48
     }
     draw() {
-        c.drawImage(this.image, this.position.x, this.position.y)
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
 }
 
+const boundaries = []
+collisionsMap.forEach((row, i) => {
+    row.forEach((symbol, j) => {
+        if (symbol === 1025)
+            boundaries.push(new Boundary({
+                position: {
+                    x: j * Boundary.width + offset.x,
+                    y: i * Boundary.height + offset.y
+                }
+            }))
+    })
+})
+
+
+class Sprite {
+    constructor({ position, velocity, image, frames = { max: 1 } }) {
+        this.position = position
+        this.image = image
+        this.frames = frames
+        this.image.onload = () => {
+            this.width = this.image.width /this.frames.max
+            this.height = this.image.height
+        }
+
+    }
+    draw() {
+
+        c.drawImage(
+            this.image,
+            0, //Cropping
+            0, //Cropping
+            this.image.width / this.frames.max, //Cropping
+            this.image.height, //Cropping
+            this.position.x,
+            this.position.y,
+            this.image.width / this.frames.max, //Actual coordinates of player 
+            this.image.height //Actual coordinates of player
+        )
+    }
+}
+
+const player = new Sprite({
+    position: {
+        x: canvas.width / 2 - 192 / 8, //Actual coordinates of player
+        y: canvas.height / 2 - (68 / 2)  //Actual coordinates of playerad
+    },
+    image: playerImage,
+    frames: {
+        max: 4
+    }
+})
+
 const background = new Sprite({
     position: {
-        x: 30,
-        y: -200
+        x: offset.x,
+        y: offset.y
     },
     image: image
 })
@@ -51,26 +115,54 @@ const keys = {
     }
 }
 
+
+const moveables = [background, ...boundaries]
+
 //Functions
+function collisionDetection({rectangle1, rectangle2}){
+    
+    return (rectangle1.position.x + rectangle1.width >= rectangle2.position.x && 
+        rectangle1.position.x <= rectangle2.position.x + rectangle2.width &&
+        rectangle1.position.y <= rectangle2.position.y + rectangle2.height &&
+        rectangle1.position.y + rectangle1.height >= rectangle2.position.y 
+    )
+}
 function animate() {
     window.requestAnimationFrame(animate) //Loops the animate function
     background.draw()
+    boundaries.forEach(boundary => {
+        boundary.draw()
+        if (collisionDetection({
+            rectangle1: player,
+            rectangle2: boundary
+        }))
+        {
+            console.log('col');
+        }
+    })
 
-    c.drawImage(
-        playerImage,
-        0, //Cropping
-        0, //Cropping
-        playerImage.width / 4, //Cropping
-        playerImage.height, //Cropping
-        canvas.width / 2 - playerImage.width / 8, //Actual coordinates of player
-        canvas.height / 2 - playerImage.height / 2,  //Actual coordinates of player
-        playerImage.width / 4, //Actual coordinates of player 
-        playerImage.height //Actual coordinates of player
-    )
-    if (keys.w.pressed && lastKey === 'w') background.position.y += 3
-    else if (keys.s.pressed && lastKey === 's') background.position.y -= 3
-    else if (keys.a.pressed && lastKey === 'a') background.position.x += 3
-    else if (keys.d.pressed && lastKey === 'd') background.position.x -= 3
+    player.draw()
+
+    if (keys.w.pressed && lastKey === 'w') {
+        moveables.forEach((moveable) => {
+            moveable.position.y += 3
+        })
+    }
+    else if (keys.s.pressed && lastKey === 's') {
+        moveables.forEach((moveable) => {
+            moveable.position.y -= 3
+        })
+    }
+    else if (keys.a.pressed && lastKey === 'a') {
+        moveables.forEach((moveable) => {
+            moveable.position.x += 3
+        })
+    }
+    else if (keys.d.pressed && lastKey === 'd') {
+        moveables.forEach((moveable) => {
+            moveable.position.x -= 3
+        })
+    }
 }
 
 function resizeCanvas() {
